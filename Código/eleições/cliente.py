3533cb3b622d1cf6362f418dc46e6a21403b8f8b
kauanmn <kauan.manzatoo@gmail.com>
2022-04-29 18:21:29 -0300

    atualização 29/04/2022
    - finalização do documento
    - revisão do código

--- Código/eleições/cliente.py
+++ Código/eleições/cliente.py
@@ -1,4 +1,6 @@
 # versão 0010
+from cryptography import x509
+from cryptography.x509.oid import NameOID
 from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.primitives import padding as sympadding # padding para cifra simétrica AES
 from cryptography.hazmat.primitives.asymmetric import ec, padding
@@ -6,6 +8,17 @@ from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from secrets import token_bytes
 from time import sleep
+
+
+def welcome(name):
+	print("*******************")
+	print("*                 *")
+	print("*  ELEIÇÕES 2042  *")
+	print("*                 *")
+	print("*******************\n\n")
+	print("Seja bem-vindo(a) às eleições digitais de 2042, Sr(a). {}\n".format(name))
+
+
 
 def get_private_key(path, passwd):
 	with open(path, 'rb') as file:
@@ -20,13 +33,13 @@ def get_private_key(path, passwd):
 def load_private_key(path):
 	for i in range(1,4):
 		try:
-			passwd = input('Insira sua senha: ').encode()
+			passwd = input('Insira seu PIN de 4 dígitos: ').encode()
 			private_key = get_private_key(path, passwd)
-			print('Chave carregada com sucesso!')
+			print('Chave carregada com sucesso.\n')
 			return private_key
 		except:
-			print('Senha incorreta. Você tem mais', 3 - i , 'tentativas.')
-	print('Você errou a senha três vezes.')
+			print('PIN incorreto. Você tem mais', 3 - i , 'tentativas.\n')
+	print('Você errou o PIN três vezes. Saindo da aplicação...')
 	exit()
 
 # dado o caminho, a função retorna o certificado x509
@@ -56,48 +69,71 @@ def aes_encrypt_data(data, key, iv):
 
 # ECDH (troca de chaves)
 def derive_key(your_private_key, their_public_key):
-        shared_key = your_private_key.exchange(ec.ECDH(), their_public_key)
-        return HKDF(
-                algorithm=hashes.SHA384(),
-                length=32,
-                salt=None,
-                info=None
-        ).derive(shared_key)
+	shared_key = your_private_key.exchange(ec.ECDH(), their_public_key)
+	return HKDF(
+		algorithm=hashes.SHA384(),
+		length=32,
+		salt=None,
+		info=None
+	).derive(shared_key)
 
 # função para escolher o voto
-def escolher_voto():
-	voto = int(input('Escolha o seu voto:')).to_bytes(length = 3, byteorder = 'big')
-	return voto
+def set_vote():
+	vote = int(input('Escolha o seu voto: '))
+	print('Seu voto é {}'.format(vote))
+	return vote.to_bytes(length = 3, byteorder = 'big')
 
-# carregar chaves públicas do TSE (RSA e EC) direto do arquivo
-print('Carregando as chaves públicas do TSE...')
-sleep(1)
+# extrair o nome do proprietário do certificado
+def get_cert_subject(cert):
+	return cert.subject.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value
 
-tse_rsa_public_key = read_public_key('./files/tse/rsa_public_key.pem')
-tse_ec_public_key  = read_public_key('./files/tse/ec_public_key.pem')
+# extrair o nome do emissor do certificado
+def get_cert_issuer(cert):
+	return cert.issuer.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value
 
 
-print('Chaves públicas carregadas.')
-sleep(1)
+
+
+
+#########################################
 
 
 # CERTIFICADO
-
 # carregar certificado digital
 print('Carregando seu certificado digital...')
+sleep(2)
+cert = read_certificate('./files/carteira/certificado.pem')
+print('Certificado carregado.\n\n')
 
-with open('./files/carteira/certificado.pem', 'rb') as file:
-	cert = file.read()
+eleitor_nome = get_cert_subject(cert) # obter o nome do eleitor pelo certificado
+welcome(eleitor_nome)
 
+
+
+# CHAVE PRIVADA DO CLIENTE
+# usada para autenticação / assinatura digital
+print('\nPara prosseguir com a votação, precisamos validar a sua identidade.')
+ct_private_key = load_private_key('./files/carteira/chave.pem') # carregar a chave privada
+ct_public_key  = ct_private_key.public_key()
+print('Pronto. Agora iremos carregar as informações necessárias...\n')
+
+
+
+# CHAVES PÚBLICAS DO TSE
+
+# carregar chaves públicas do TSE (RSA e EC) direto do arquivo
+print('Carregando as chaves públicas do TSE...')
+sleep(2)
+tse_rsa_public_key = read_public_key('./files/tse/rsa_public_key.pem')
+tse_ec_public_key  = read_public_key('./files/tse/ec_public_key.pem')
+print('Chaves públicas carregadas.\n')
 sleep(1)
-print('Certificado carregado.')
-sleep(1)
-
 
 
 # VOTO
-voto = escolher_voto()
-print('Criptografando seu voto...')
+voto = set_vote()
+sleep(1)
+print('\nCriptografando seu voto...')
 sleep(1)
 
 # criptografar o voto usando RSA
@@ -110,15 +146,15 @@ voto_secreto = tse_rsa_public_key.encrypt(
 	)
 )
 sleep(2)
-print('O voto foi criptografado com sucesso!')
+print('O voto foi criptografado com sucesso.\n')
 
 
 
 # ASSINATURA
-ct_private_key = load_private_key('./files/carteira/chave.pem')
+
 
 # assinar o voto secreto usando ECDSA
-print('Assinando digitalmente...')
+print('Assinando seu voto...')
 sleep(1)
 
 assinatura = ct_private_key.sign(
@@ -126,7 +162,7 @@ assinatura = ct_private_key.sign(
 	ec.ECDSA(hashes.SHA384())
 )
 
-print('Assinatura digital feita com sucesso!')
+print('Assinatura digital feita com sucesso.\n')
 sleep(1)
 
 
@@ -148,15 +184,26 @@ iv = token_bytes(16)
 # initialization vector (IV), não é uma informação pública
 # o IV deve ser único para cada par (mensagem, chave)
 
+
+cert_pem = cert.public_bytes(encoding=serialization.Encoding.PEM) # certificado
+ct_public_key_pem = ct_public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo)
+
+
+# criptografar os arquivos usando AES e a chave derivada do ECDH
 enc_voto_secreto = aes_encrypt_data(voto_secreto, derived_key, iv)
 enc_assinatura   = aes_encrypt_data(assinatura,   derived_key, iv)
-enc_cert         = aes_encrypt_data(cert,         derived_key, iv)
+enc_cert         = aes_encrypt_data(cert_pem,     derived_key, iv)
 
 
 # simulação do envio
 print('Enviando os dados...')
 sleep(2)
 save_file(enc_voto_secreto, 'enc_voto_secreto')
-save_file(enc_assinatura, 'enc_assinatura')
-save_file(cert, 'certificado.pem')
+save_file(enc_assinatura,   'enc_assinatura')
+save_file(enc_cert,         'enc_cert')
+save_file(ct_public_key_pem,'chave_publica_eleitor.pem')
 save_file(iv, 'iv') # o IV é uma informação pública usada na cifra AES-CBC
+
+print('\nPronto. Seu voto foi enviado ao TSE. Agora é só esperar os resultados.')
